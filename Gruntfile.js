5e3ed4b5aced2ff3fc04e9a827bc62dacf62ec68
Gabriele Rabbiosi <gabriele.rabbiosi@gmail.com>
2013-04-07 15:57:53 +0200

    Added grunt configuration for jshint, concat and uglify

---
+++ Gruntfile.js
@@ -0,0 +1,58 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    kendoVersion: '2013.1.319',
+    pkg: grunt.file.readJSON('package.json'),
+    filename: 'kiui',
+    commons: {
+      banner: '/*\n' +
+              ' * <%= pkg.name %> v<%= pkg.version %> (https://github.com/redaemn/KiUI)\n' +
+              ' * <%= grunt.template.today("yyyy-mm-dd") %>\n' +
+              ' * Author: <%= pkg.author %>\n' +
+              ' */\n'
+    },
+    concat: {
+      dist: {
+        options: {
+          banner: '<%= commons.banner %>'
+        },
+        src: ['src/**/*.js'],
+        dest: 'dist/<%= filename %>-<%= pkg.version %>.js'
+      }
+    },
+    uglify: {
+      dist:{
+        options: {
+          banner: '<%= commons.banner %>'
+        },
+        src:['dist/<%= filename %>-<%= pkg.version %>.js'],
+        dest:'dist/<%= filename %>-<%= pkg.version %>.min.js'
+      }
+    },
+    jshint: {
+      files: ['Gruntfile.js','src/**/*.js'],
+      options: {
+        curly: true,
+        immed: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        boss: true,
+        eqnull: true,
+        globals: {
+          jQuery: true,
+          kendo: true
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  // Default task.
+  grunt.registerTask('default', ['jshint', 'concat', 'uglify']);
+
+};
