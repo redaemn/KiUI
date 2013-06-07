a7cf7364a53dddf4d6c2aefb5cddfd77847974f6
Gabriele Rabbiosi <gabriele.rabbiosi@gmail.com>
2013-06-08 01:26:27 +0200

    feat(utilities-notify): update demo

--- demo/utilities/notify/demo.js
+++ demo/utilities/notify/demo.js
@@ -1,11 +1,27 @@
 jQuery(function($) {
 
-  var notifier = kiui.notify();
+  var notifier = kiui.notifier();
 
   function notifyError() {
-    notifier.error("this is an error");
+    notifier.error({
+      html: "this is an error"
+    });
+  }
+  
+  function notifyInfo() {
+    notifier.info({
+      html: "this is an info"
+    });
+  }
+  
+  function notifySuccess() {
+    notifier.success({
+      html: "this is a success"
+    });
   }
 
   $('#utilities_notify_demo button#error').click(notifyError);
+  $('#utilities_notify_demo button#info').click(notifyInfo);
+  $('#utilities_notify_demo button#success').click(notifySuccess);
 
 });
