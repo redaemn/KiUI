eca817cfde31bdc7152dcc26ab151889f1f24753
Gabriele Rabbiosi <gabriele.rabbiosi@gmail.com>
2013-09-24 00:17:59 +0200

    feat(widgets-popupMenu): start implementing the demo

--- demo/widgets/popupMenu/demo.js
+++ demo/widgets/popupMenu/demo.js
@@ -1,6 +1,36 @@
 jQuery(function($) {
 
-  $('#widgets_popupMenu_demo .ex1').kendoKiuiPopupMenu();
-  $('#widgets_popupMenu_demo .ex2').kendoKiuiPopupMenu();
+  // basic layout needed
+  $('#widgets_popupMenu_demo .basic-layout').kendoKiuiPopupMenu();
+  
+  // layout using a <span> and an <a>
+  $('#widgets_popupMenu_demo .alternative-layout').kendoKiuiPopupMenu();
+  
+  // event handling
+  
+  // openOnHover
+  $('#widgets_popupMenu_demo .openOnHover-option').kendoKiuiPopupMenu({
+    openOnHover: true
+  });
+  
+  //direction: 'bottom right', // can also be 'bottom left', 'top right' or 'top left'
+  
+  //animation: 'expand', // can also be 'fade' or 'none'
+  
+  // kendoMenu is accessible using the widget's "menu" property
+  
+  
+  
+  /*var blockOpen = false;
+  
+  $('#widgets_popupMenu_demo .ex2').data('kendoKiuiPopupMenu').bind('open', function(e) {
+    console.log(e.item);
+    if (blockOpen) {
+      console.log('prevented');
+      e.preventDefault();
+    }
+    
+    blockOpen = !blockOpen;
+  });*/
 
 });
