8028e0781916b322627cb7e43d131febe3fd857e
Gabriele Rabbiosi <gabriele.rabbiosi@gmail.com>
2013-09-24 22:02:27 +0200

    feat(widgets-popupMenu): update the demo

--- demo/widgets/popupMenu/demo.js
+++ demo/widgets/popupMenu/demo.js
@@ -14,6 +14,21 @@ jQuery(function($) {
   });
   
   //direction: 'bottom right', // can also be 'bottom left', 'top right' or 'top left'
+  $('#widgets_popupMenu_demo .bottom-right-direction-option').kendoKiuiPopupMenu({
+    direction: 'bottom right'
+  });
+  
+  $('#widgets_popupMenu_demo .bottom-left-direction-option').kendoKiuiPopupMenu({
+    direction: 'bottom left'
+  });
+  
+  $('#widgets_popupMenu_demo .top-right-direction-option').kendoKiuiPopupMenu({
+    direction: 'top right'
+  });
+  
+  $('#widgets_popupMenu_demo .top-left-direction-option').kendoKiuiPopupMenu({
+    direction: 'top left'
+  });
   
   //animation: 'expand', // can also be 'fade' or 'none'
   
