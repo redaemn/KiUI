2384ea73198864b8e339231e074957eda4c75267
Gabriele Rabbiosi <gabriele.rabbiosi@gmail.com>
2013-10-11 17:59:13 -0400

    feat(validators-date): update demo examples

--- demo/validators/date/demo.js
+++ demo/validators/date/demo.js
@@ -1,7 +1,27 @@
 jQuery(function($) {
   
-  $('#validators_dateValue_demo .datePicker').kendoDatePicker({
-    //format: 'dd-MM-yyyy'
+  // set Italian culture
+  kendo.culture("it-IT");
+  // reset default US culture
+  kendo.culture("en-US");
+  // I need to do this because there seems to be a bug in kendo.parseDate(): it
+  // doesn't work for cultures that have never been set as default culture at
+  // least one time
+  
+  var today = new Date();
+  
+  $('#validators_dateValue_demo [name="date"]').kendoDatePicker({
+    value: today
+  });
+  
+  $('#validators_dateValue_demo [name="date-format"]').kendoDatePicker({
+    format: 'dd-MM-yyyy',
+    value: today
+  });
+  
+  $('#validators_dateValue_demo [name="date-culture"]').kendoDatePicker({
+    culture: 'it-IT',
+    value: today
   });
   
   var validator = $("#validators_dateValue_demo form").kendoValidator().data("kendoValidator");
