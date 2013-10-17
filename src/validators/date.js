71b12e0b3209114cb9b3e5b24a1d0a85f42e60fd
Gabriele Rabbiosi <gabriele.rabbiosi@gmail.com>
2013-10-17 17:20:38 -0400

    feat(validators-date): fix windows newline mess and modify validator key

--- src/validators/date.js
+++ src/validators/date.js
@@ -1,1 +1,24 @@
-/** * Check that the input field contains a valid date value based on a particular * format and/or culture */(function ($, kendo, undefined) {    kendo.ui.Validator.prototype.options.rules =    $.extend(kendo.ui.Validator.prototype.options.rules, {      date: function (input) {        if (input.is('[data-validation-date]')) {          var value = input.val(),            format = input.data('validation-date') || "",            culture = input.data('validation-date-culture') || "";                      return value === "" ||            kendo.parseDate(value, format, culture) !== null;        }        return true;      }    });  })(window.jQuery, window.kendo);
+/**
+ * Check that the input field contains a valid date value based on a particular
+ * format and/or culture
+ */
+
+(function ($, kendo, undefined) {
+  
+  kendo.ui.validator.rules.kiuiDate = function (input) {
+    if (input.is('[data-kiui-date]')) {
+      var value = input.val(),
+        format = input.data('kiui-date') || "",
+        culture = input.data('kiui-date-culture') || "";
+        
+      return value === "" ||
+        kendo.parseDate(value, format, culture) !== null;
+    }
+
+    return true;
+  };
+  
+  kendo.ui.validator.messages.kiuiDate = "{0} is not a valid date";
+  
+})(window.jQuery, window.kendo);
+
