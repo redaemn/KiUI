8b20c4425619303e842c3168ba7bed5c191b4305
Gabriele Rabbiosi <gabriele.rabbiosi@gmail.com>
2013-10-21 16:08:46 -0400

    feat(validators-radioRequired): add first implementation

---
+++ src/validators/radioRequired.js
@@ -0,0 +1,19 @@
+/**
+ * Extends require validator to handle radio buttons inputs
+ */
+
+(function ($, kendo, undefined) {
+  
+  kendo.ui.validator.rules.required = function (input) {
+    var validator;
+    
+    if (input.is("[type=radio]")) {
+      validator = input.parents('[data-role="validator"]').first();
+      return !input.is("[required]") || validator.find("[name=" + input.attr("name") + "]").is(":checked");
+    }
+    else {
+      return kendo.ui.Validator.prototype.options.rules.required(input);
+    }
+  };
+  
+})(window.jQuery, window.kendo);
