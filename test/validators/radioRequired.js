b47fbff36ca8ed9913eb9f314932627b3644b203
Gabriele Rabbiosi <gabriele.rabbiosi@gmail.com>
2013-10-21 16:37:14 -0400

    feat(validators-radioRequired): add some tests

---
+++ test/validators/radioRequired.js
@@ -0,0 +1,90 @@
+describe('validators radioRequired:', function() {
+  
+  function validate(elem) {
+    return kendo.ui.validator.rules.required(elem);
+  }
+  
+  it('should be present among the standard kendo validators', function() {
+    expect(kendo.ui.validator.rules.required).toEqual(jasmine.any(Function));
+  });
+  
+  describe('with radio buttons', function() {
+    
+    var validator, radio1, radio2;
+    
+    beforeEach(function() {
+      validator = $('<div data-role="validator"></div>').appendTo($('body'));
+      radio1 = $('<input name="inputName" type="radio" value="1" required />').appendTo(validator);
+      radio2 = $('<input name="inputName" type="radio" value="2" required />').appendTo(validator);
+    });
+    
+    afterEach(function() {
+      validator.remove();
+      validator = radio1 = radio2 = undefined;
+    });
+    
+    it('should return true if the radio buttons are not required', function() {
+      radio1.prop('required', false);
+      radio2.prop('required', false);
+      
+      expect(validate(radio1)).toBe(true);
+      expect(validate(radio2)).toBe(true);
+    });
+    
+    it('should return false if the radio buttons are required and none is checked', function() {
+      expect(validate(radio1)).toBe(false);
+      expect(validate(radio2)).toBe(false);
+    });
+    
+    it('should return true if the radio buttons are required and the first one is checked', function() {
+      radio1.prop('checked', true);
+      
+      expect(validate(radio1)).toBe(true);
+      expect(validate(radio2)).toBe(true);
+    });
+    
+    it('should return true if the radio buttons are required and the second one is checked', function() {
+      radio2.prop('checked', true);
+      
+      expect(validate(radio1)).toBe(true);
+      expect(validate(radio2)).toBe(true);
+    });
+    
+    describe('together with other radio buttons', function() {
+      
+      var otherRadio1, otherRadio2;
+      
+      beforeEach(function() {
+        otherRadio1 = $('<input name="otherInputName" type="radio" value="1" />').appendTo(validator);
+        otherRadio2 = $('<input name="otherInputName" type="radio" value="2" />').appendTo(validator);
+      });
+      
+      afterEach(function() {
+        otherRadio1 = otherRadio2 = undefined;
+      })
+      
+      it('should return false if the radio buttons are required and a different radio button is checked', function() {
+        otherRadio1.prop('checked', true);
+        
+        expect(validate(radio1)).toBe(false);
+        expect(validate(radio2)).toBe(false);
+      });
+      
+      it('should return true if the radio buttons are required, the first one is checked and none of the other radio buttons are checked', function() {
+        radio1.prop('checked', true);
+        
+        expect(validate(radio1)).toBe(true);
+        expect(validate(radio2)).toBe(true);
+      });
+      
+    });
+    
+  });
+  
+  describe('with input[type="text"] elements', function() {
+    
+    
+    
+  });
+  
+});
