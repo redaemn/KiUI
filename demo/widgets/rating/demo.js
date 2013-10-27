b613583f19eb802390bdd5ae59141afd508170c8
Gabriele Rabbiosi <gabriele.rabbiosi@gmail.com>
2013-10-27 16:24:05 -0400

    feat(widget-rating): add some demos

--- demo/widgets/rating/demo.js
+++ demo/widgets/rating/demo.js
@@ -1,13 +1,53 @@
 jQuery(function($) {
 
-  
+  var viewModel,
+    ratingWidget;
 
-  $('#widgets_rating_demo .star-rating').kendoKiuiRating({});
+  ratingWidget = $('#widgets_rating_demo .star-rating').kendoKiuiRating({
+    mouseover: function(e) {
+      console.log(e);
+      var title = e.item.attr('title');
+      $('#widgets_rating_demo .star-hover').text(" - " + e.value + ": " + title);
+    },
+    mouseleave: function() {
+      $('#widgets_rating_demo .star-hover').text("");
+    }
+  }).data('kendoKiuiRating');
 
-  window.viewModel = kendo.observable({
-    rating: 0
+  viewModel = kendo.observable({
+    rating: null,
+    readonly: false,
+    toggleReadonly: function() {
+      this.readonly = !this.readonly;
+      
+      if (this.readonly) {
+        this.set('toggleReadonlyText', 'readonly');
+        this.set('toggleReadonlyClass', 'btn btn-danger');
+      }
+      else {
+        this.set('toggleReadonlyText', 'read-write');
+        this.set('toggleReadonlyClass', 'btn');
+      }
+      
+      ratingWidget.readonly(this.readonly);
+    },
+    toggleReadonlyText: "read-write",
+    toggleReadonlyClass: "btn"
   });
 
-  kendo.bind($('#widgets_rating_demo'), window.viewModel);
+  kendo.bind($('#widgets_rating_demo'), viewModel, kendo.ui, kiui);
+  
+  $('#widgets_rating_demo .custom-star-rating').kendoKiuiRating({
+    starEmptyClass: "icon-check-empty",
+    starFullClass: "icon-check",
+    mouseover: function(e) {
+      console.log(e);
+      var title = e.item.attr('title');
+      $('#widgets_rating_demo .custom-star-hover').text(" - " + e.value + ": " + title);
+    },
+    mouseleave: function() {
+      $('#widgets_rating_demo .custom-star-hover').text("");
+    }
+  });
   
 });
